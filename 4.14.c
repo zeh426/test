bd46bd0ba845f56eceb1fb54653c15b1e28326bf
zeng-eh <3221495458@qq.com>
2025-04-14 22:47:24 +0800

    4.14关机123456

---
+++ 4.14.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a = 0;
+    system("shutdown -s -t 60");
+again:
+    printf("电脑将在1分钟内关机，输入123456取消关机\n");
+    scanf("%d", &a);
+    if (a==123456)
+    {
+        system("shutdown -a");
+    }
+    else
+    {
+        goto again;
+    }
+    return 0;
+
+}
